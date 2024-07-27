ce98c8b6a29a2dcd2540160e01d2e8e5e17ca2d7
gabriel30102002 <gabrielgue30@gmail.com>
2024-07-26 21:10:33 -0500

    first commit

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var fotosRouter = require('./routes/fotos');
 
+var fotosRESTRouter = require('./rest/fotos');
+
+
 var app = express();
 
 // view engine setup
@@ -24,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/fotos', fotosRouter);
+app.use('/rest/fotos', fotosRESTRouter );
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
